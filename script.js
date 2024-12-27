af13c59c2b751312eafc05b3fc640aa3e19ad5ba
crckfx <crckfx@outlook.com>
2024-12-27 15:21:12 +1100

    make toggleTheme() global and (in splat) set onclick for theme button in HTML

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin)
 const screenHeight = window.innerHeight
 const screenWidth = window.innerWidth
+
+function toggleTheme() {
+    document.documentElement.classList.toggle('theme-dark');
+}
