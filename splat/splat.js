7514e4c1a56852424ba71f6d9154dbadf674772e
crckfx <crckfx@outlook.com>
2024-12-27 12:19:25 +1100

    demonstrate 'dark' class on the body to style child elements in splat

--- splat/splat.js
+++ splat/splat.js
@@ -2,16 +2,23 @@ const swapMode = document.querySelector(".buttonFloat")
 const styleContainer = document.querySelector(".styleContainer")
 const styleHeader = document.querySelector(".styleHeader")
 
+const body = document.querySelector('body');
+
 
 swapMode.addEventListener("click", () => {
-    if (swapMode.textContent == "Dark Mode") {
-        swapMode.textContent = "Light Mode"
-        styleContainer.style.backgroundColor = "#1A202C";
-        styleHeader.style.backgroundColor = "#4299E1";
+    if (!body.classList.contains('dark')) {
+        body.classList.add('dark');
+        // enter dark mode
+        swapMode.textContent = "Light Mode"        
+        // styleHeader.classList.add('dark');
+        // styleContainer.classList.add('dark');
         
     } else {
+        body.classList.remove('dark');
+        // enter light mode
         swapMode.textContent = "Dark Mode"
-        styleHeader.style.backgroundColor = "#0077CC";
-        styleContainer.style.backgroundColor = "#F9FAFB";
+        // styleHeader.classList.remove('dark');
+        // styleContainer.classList.remove('dark');
+
     }
 })
