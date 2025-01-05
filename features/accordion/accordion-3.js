4bc6baec0078f9fc670756d8cc3f81fa80d19bb0
crckfx <crckfx@outlook.com>
2025-01-05 22:14:55 +1100

    new strategy for accordion 'light'

--- features/accordion/accordion-3.js
+++ features/accordion/accordion-3.js
@@ -1,32 +1,43 @@
 // adapted from https://codepen.io/kevinpowell/pen/NWOgVga
 //          and https://keithjgrant.com/posts/2023/04/transitioning-to-height-auto/
 
-const accordion3 = document.querySelector(".accordion-3");
+const accordions = document.querySelectorAll(".accordion");
+for (let i=0; i<accordions.length; i++) {
+    createAccordion(accordions[i]);
+}
 
-accordion3.addEventListener("click", (e) => {
-    const activePanel = e.target.closest(".accordion-panel");
-    if (!activePanel) return;
-    toggleAccordion(activePanel);
-});
+function createAccordion(accordion) {
 
-function toggleAccordion(panelToActivate) {
-    const activeButton = panelToActivate.querySelector("button");
-    const activePanel = panelToActivate.querySelector(".accordion-content");
-    const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
+    accordion.addEventListener("click", (e) => {
+        // Ensure only clicks on the top/header trigger the toggle
+        const header = e.target.closest(".accordion-trigger");
+        if (!header) return;
 
-    if (activePanelIsOpened === "true") {
-        panelToActivate
-            .querySelector("button")
-            .setAttribute("aria-expanded", false);
+        const activePanel = e.target.closest(".accordion-panel");
+        if (!activePanel) return;
+        toggleAccordion(activePanel);
+    });
 
-        panelToActivate
-            .querySelector(".accordion-content")
-            .setAttribute("aria-hidden", true);
-    } else {
-        panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+    function toggleAccordion(panelToActivate) {
+        const activeButton = panelToActivate.querySelector("button");
+        const activePanel = panelToActivate.querySelector(".accordion-content");
+        const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
 
-        panelToActivate
-            .querySelector(".accordion-content")
-            .setAttribute("aria-hidden", false);
+        if (activePanelIsOpened === "true") {
+            panelToActivate
+                .querySelector("button")
+                .setAttribute("aria-expanded", false);
+
+            panelToActivate
+                .querySelector(".accordion-content")
+                .setAttribute("aria-hidden", true);
+        } else {
+            panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+
+            panelToActivate
+                .querySelector(".accordion-content")
+                .setAttribute("aria-hidden", false);
+        }
     }
+
 }
