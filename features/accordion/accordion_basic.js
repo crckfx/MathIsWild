1d736138d8d41601b9838249c37ba1f1964fbef4
crckfx <crckfx@outlook.com>
2025-01-28 22:44:05 +1100

    rename old accordion docs to prepare for class-based method

--- features/accordion/accordion_basic.js
+++ features/accordion/accordion_basic.js
@@ -1,13 +1,14 @@
 // adapted from https://codepen.io/kevinpowell/pen/NWOgVga
 //          and https://keithjgrant.com/posts/2023/04/transitioning-to-height-auto/
 
+// automatically find any accordions and "create" them
 const accordions = document.querySelectorAll(".accordion");
 for (let i=0; i<accordions.length; i++) {
     createAccordion(accordions[i]);
 }
 
+// function to create accordion from a div
 function createAccordion(accordion) {
-
     accordion.addEventListener("click", (e) => {
         // Ensure only clicks on the top/header trigger the toggle
         const header = e.target.closest(".accordion-trigger");
@@ -18,23 +19,22 @@ function createAccordion(accordion) {
         toggleAccordion(activePanel);
     });
 
-    function toggleAccordion(panelToActivate) {
-        const activeButton = panelToActivate.querySelector("button");
-        const activePanel = panelToActivate.querySelector(".accordion-content");
-        const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
+    function toggleAccordion(currentPanel) {
+        const panelButton = currentPanel.querySelector("button");
+        const panelIsOpened = panelButton.getAttribute("aria-expanded");
 
-        if (activePanelIsOpened === "true") {
-            panelToActivate
+        if (panelIsOpened === "true") {
+            currentPanel
                 .querySelector("button")
                 .setAttribute("aria-expanded", false);
 
-            panelToActivate
+            currentPanel
                 .querySelector(".accordion-content")
                 .setAttribute("aria-hidden", true);
         } else {
-            panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+            currentPanel.querySelector("button").setAttribute("aria-expanded", true);
 
-            panelToActivate
+            currentPanel
                 .querySelector(".accordion-content")
                 .setAttribute("aria-hidden", false);
         }
